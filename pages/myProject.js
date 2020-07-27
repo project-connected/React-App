10e01dc3afa14dfbd67e503565228157323d3c02
GitSeob <anhs0220@gmail.com>
2020-07-27 20:04:12 +0900

    페이지 라우트에 필요한 파일 구조 추가

---
+++ pages/myProject.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const MyProject = props => {
+	return (
+		<div>
+
+		</div>
+	);
+};
+
+MyProject.propTypes = {
+
+};
+
+export default MyProject;
---
+++ pages/signup.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Signup = props => {
+	return (
+		<div>
+
+		</div>
+	);
+};
+
+Signup.propTypes = {
+
+};
+
+export default Signup;
