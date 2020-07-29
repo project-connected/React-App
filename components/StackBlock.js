14fff7631149e739735f94b1e48100b4456ce3ea
GitSeob <anhs0220@gmail.com>
2020-07-29 16:43:01 +0900

    기술 스택, 툴 블럭 디자인, project card에 test 디자인

--- components/StackBlock.js
+++ components/StackBlock.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const StackBlock = props => {
+const StackBlock = ({ color='black', name }) => {
 	return (
-		<>
-		</>
+		<span className='stack-block' style={{
+			background: color
+		}}>
+			{name}
+		</span>
 	);
 };
 
