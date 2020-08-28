df993299ad7844cc43f9f4591e4996b7e2c18885
GitSeob <anhs0220@gmail.com>
2020-08-28 14:36:01 +0900

    인재풀 페이지 init

---
+++ pages/jewel/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FindJewel = props => {
+	return (
+		<div>
+
+		</div>
+	);
+};
+
+FindJewel.propTypes = {
+
+};
+
+export default FindJewel;
---
+++ pages/jewel/new.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CreateMyAppeal = props => {
+	return (
+		<div>
+
+		</div>
+	);
+};
+
+CreateMyAppeal.propTypes = {
+
+};
+
+export default CreateMyAppeal;
