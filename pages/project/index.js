54f493f014834e6e5a58c24ae67a071b6bf1ee8e
GitSeob <anhs0220@gmail.com>
2020-10-08 12:06:38 +0900

    /project filter header 반응형 디자인 추가

--- pages/project/index.js
+++ pages/project/index.js
@@ -100,34 +100,6 @@ const SearchProj = props => {
 
 	return (
 		<div className="proj-search-page">
-			<div className="proj-etc-wrap">
-				<Link href="/project/create">
-					<a>
-						<div id="create">
-							프로젝트 만들기 버튼
-						</div>
-					</a>
-				</Link>
-				<Link href="/">
-					<a>
-						<div id="apply">
-							인재풀 등록 버튼
-						</div>
-					</a>
-				</Link>
-				<div className="etc-link">
-					<Link href="/">
-						<a>
-							마감 임박
-						</a>
-					</Link>
-					<Link href="/">
-						<a>
-							관심있을 프로젝트
-						</a>
-					</Link>
-				</div>
-			</div>
 			<div className="proj-search-wrap">
 				<div className="search-filter-box">
 					<h3>검색 필터링</h3>
