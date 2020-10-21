f2cc30fd22bee13637a7e7147866c621a2326fa6
GitSeob <anhs0220@gmail.com>
2020-10-21 20:46:53 +0900

    highlight cdn in Head

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,30 @@
-import React, { useEffect } from "react";
-import PropTypes from "prop-types";
-import Head from "next/head";
-import wrapper from "../store/configureStore";
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import wrapper from '../store/configureStore';
 
 // css import
-import "@toast-ui/editor/dist/toastui-editor.css";
-import "@toast-ui/editor/dist/toastui-editor-viewer.css";
-import "codemirror/lib/codemirror.css";
-import "aos/dist/aos.css";
-import "bootstrap/dist/css/bootstrap.css";
-import "react-calendar/dist/Calendar.css";
-import "../css/default.scss";
-import "../css/design.scss";
-import "../css/layout.scss";
-import "../css/mainPage.scss";
-import "../css/signup.scss";
-import "../css/chat.scss";
-import "../css/project.scss";
-import "../css/forms.scss";
-import "../css/buttons.scss";
-import "../css/jewel.scss";
-import "../css/profile.scss";
-import "../css/material-ui.scss";
+import '@toast-ui/editor/dist/toastui-editor.css';
+import '@toast-ui/editor/dist/toastui-editor-viewer.css';
+import 'tui-color-picker/dist/tui-color-picker.css';
+import 'codemirror/lib/codemirror.css';
+import 'aos/dist/aos.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'react-calendar/dist/Calendar.css';
+import '../css/default.scss';
+import '../css/design.scss';
+import '../css/layout.scss';
+import '../css/mainPage.scss';
+import '../css/signup.scss';
+import '../css/chat.scss';
+import '../css/project.scss';
+import '../css/forms.scss';
+import '../css/buttons.scss';
+import '../css/jewel.scss';
+import '../css/profile.scss';
+import '../css/material-ui.scss';
 
-import AppLayout from "../containers/AppLayout";
+import AppLayout from '../containers/AppLayout';
 
 const Connected = ({ Component }) => {
 	return (
@@ -39,6 +40,12 @@ const Connected = ({ Component }) => {
 					rel="stylesheet"
 					href="https://fonts.googleapis.com/css?family=Permanent+Marker"
 				/>
+				<link
+					rel="stylesheet"
+					href="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.1.2/build/styles/default.min.css"
+				/>
+				<script src="//cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.1.2/build/highlight.min.js"></script>
+				<script>hljs.initHighlightingOnLoad();</script>
 			</Head>
 			<AppLayout>
 				<Component />
